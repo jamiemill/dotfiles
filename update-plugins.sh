57a4065ab790bee07a3458f927b3bf2fe895a0b1
Jamie Mill <jamiermill@gmail.com>
2013-11-19 19:12:37 +0000

    Prevent update-plugins from using paralellism, and print hint afterwards.
    I think the paralellism is causing ssh key password requests to be
    swallowed, and also I can't print the message afterwards without knowing
    when all the subshells have completed.

--- update-plugins.sh
+++ update-plugins.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-git submodule foreach "(git checkout master; git pull)&"
+git submodule foreach "git checkout master; git pull"
+
+echo "#######################################################"
+echo "# Submodules updated. Now commit and run ./install.sh #"
+echo "# on other machines to update.                        #"
+echo "#######################################################"
