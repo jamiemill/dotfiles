d202f3e39a7dfc34b39ab6f07d310e084d073075
Jamie Mill <jamiermill@gmail.com>
2014-06-05 00:25:44 +0100

    Start my theme

---
+++ vim-plugins/jamietheme/jamietheme.rb
@@ -0,0 +1,49 @@
+pink = "f25"
+lime = "3d2"
+turquoise = "0aa"
+orange = "c60"
+grey = "444"
+blue = "24f"
+
+# Comment
+
+vim_colors "jamie" do
+  author "Jamie Mill"
+  notes  "Jamie's theme"
+
+  reset      true
+  background :dark
+
+  Normal "999", "111"
+
+  Folded "808080", "ECECEC", :gui => "bold"
+  link :vimFold, :FoldColumn, :to => :Folded
+  LineNr grey, "222"
+
+  rubyControl lime
+  rubySymbol pink
+  Comment grey
+  rubyComment grey
+
+  Statement orange
+  Boolean orange
+  rubyBoolean orange
+  Number lime
+
+
+  Constant turquoise
+  rubyString turquoise
+  rubyStringDelimiter turquoise
+
+  Special grey
+  Operator lime
+
+  Type blue
+  rubyConstant blue
+  jsFuncParens blue
+  jsFuncBraces blue
+  #jsFuncCall pink
+  jsParens pink
+
+end
+
