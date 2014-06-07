9332919be551f5109bef1dfb49eda53b27298a7b
Jamie Mill <jamiermill@gmail.com>
2014-06-07 18:28:43 +0100

    Print failures to regenerate colorscheme to the screen

--- vim-plugins/jamietheme/regenerate.sh
+++ vim-plugins/jamietheme/regenerate.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-# Must be run from the theme directory
+# Must be run from the theme directory.
+
+# Palette prints errors to stdout so errors end up replacing
+# the scheme. If it fails we `cat` the contents of the scheme so you
+# can see the errors.
 
 echo Regenerating...
+
 palette jamietheme.rb > colors/jamietheme.vim
-echo Done.
+
+if [ $? = 0 ]; then
+    echo Success!
+else
+    echo Failed. Errors should be below:
+    echo
+    cat colors/jamietheme.vim
+    exit $?
+fi
