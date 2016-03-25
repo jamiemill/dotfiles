b9189e64bc902b62120fe086fd6a502deb3c198a
Jamie Mill <jamiermill@gmail.com>
2016-03-25 22:00:59 +0000

    Port IE vm function to fish

---
+++ fish/functions/ie.fish
@@ -0,0 +1,7 @@
+function ie
+    set IE_VERSION $argv[1]
+    set VNCBIN "/Applications/Chicken of the VNC.app/Contents/MacOS/Chicken of the VNC"
+    set JOB "http://jenkins.youdevise.com/job/qa-browsers-vnc-report/lastBuild/logText/progressiveText"
+    set TARGET (curl -s $JOB | grep "qa-a-ie"$IE_VERSION"-001" | rev | cut -d " " -f 1 | rev)
+    eval \"$VNCBIN\" $TARGET
+end
