4af21d345a3feb1f26175ca8103fd0b73d19a46e
Jamie Mill <jamiermill@gmail.com>
2017-06-19 18:15:06 +0100

    Debug vnc target

--- fish/functions/ie.fish
+++ fish/functions/ie.fish
@@ -3,5 +3,6 @@ function ie
     set VNCBIN "/Applications/Chicken of the VNC.app/Contents/MacOS/Chicken of the VNC"
     set JOB "http://jenkins.youdevise.com/job/qa-browsers-vnc-report/lastBuild/logText/progressiveText"
     set TARGET (curl -s $JOB | grep "qa-a-ie"$IE_VERSION"-001" | rev | cut -d " " -f 1 | rev)
+    echo "Found $TARGET"
     eval \"$VNCBIN\" $TARGET
 end
